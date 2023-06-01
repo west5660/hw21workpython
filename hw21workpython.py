898cf774917b2317c1a642b48d17f057becde175
west5660 <104725439+west5660@users.noreply.github.com>
2023-06-01 23:06:21 +0300

    Add files via upload

---
+++ hw21workpython.py
@@ -0,0 +1,77 @@
+# int целое число
+# float дробное число
+# str строка
+# bool логическое
+# for while циклы
+# if else проверка
+# [] list массивы
+# {} dict словарь массив
+# () tuole массив который нельзя менять
+# def f1():  функция
+# import библеотека
+# print("hello epta")
+# num = 0
+# print(num+1)
+# num = 12
+# print(num+1)
+#
+# sut = 24
+# hour = 60
+# minute = 60
+# result = sut*minute*hour
+# print('секунд в сутках', result)
+# print('секунд в сутках '+ str(result))
+# a = 1
+# print(bool(a))
+# a = 4
+# cat = 3
+# b = 7
+# c = a*b*3
+# print(c)
+# print(f'коты едят за неделю {c}')
+#
+# v = 100
+# h = 3600
+# m = 1000
+# i = v*m/h
+# print(float(i))
+#
+# name = input('name?\n')
+# print('hello', name)
+# film = input('напиши фильм\n')
+# eda = input('какая еда\n')
+# print(f'Привет {name} давай возьмем много {eda} и посмотрим {film}')
+#
+# num1 = input('number1\n')
+# num2 = input('number2\n')
+# print(int(num1)*int(num2))
+
+# print(13%10)
+# b = 2024
+#
+# a = int(input('год рождения\n'))
+# print(b-a)
+# input()
+print('Задача1')
+kmday = 2
+kmdayh = 5
+day = 5
+holyday = 2
+nday = 7
+a1 = kmday*day
+a2 = kmdayh*holyday
+result = a1+a2
+print(f'вы пройдете за неделю {result} km')
+
+print('ЗАдача2')
+c = int(input('Назовите длину стороны кубика\n'))
+b = c**3
+print(f'объем кубика равен {b} m3')
+
+print('Зaдача3')
+b1 = int(input('Сколько вы зарабатываете\n'))
+b2 = int(input('Какой платеж по кредиту\n'))
+b3 = int(input('Сколько нужно заплатить за коммуналку\n'))
+b4 = b1-b2-b3
+print(f'Денег после всех оплат у вас останется {b4} тыс. рублей')
+
